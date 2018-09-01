645b00c8d6a9c4f86d33fefad70a9e668ea2d55c
AustinDev16 <austin.blaser@gmail.com>
2018-09-01 12:34:59 -0600

    Added custom view controller and made it entry point of app.

--- Weather Stop/AppDelegate.swift
+++ Weather Stop/AppDelegate.swift
@@ -16,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Launch custom view controller instead of story board
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = MainWeatherViewController()
+        let nc = UINavigationController(rootViewController: vc)
+        
+        window.rootViewController = nc
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        UIApplication.shared.keyWindow?.backgroundColor = .white
         return true
     }
 
--- Weather Stop/View Controllers/MainWeatherViewController.swift
+++ Weather Stop/View Controllers/MainWeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainWeatherViewController.swift
 //  Weather Stop
 //
 //  Created by Austin Blaser on 9/1/18.
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainWeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        // Do any additional setup after loading the view.
         let testQuery = "select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text=\"(40.7141667,-74.0063889)\")"
         WeatherObjectController.fetchWeather(withYQLQuery: testQuery) { (weatherObj, error) in
             if (error != nil) {
@@ -26,7 +26,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
