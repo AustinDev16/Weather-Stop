153c98b934fc0cab9685c232b6b8f519075d418e
AustinDev16 <austin.blaser@gmail.com>
2018-09-01 10:18:03 -0600

    Initial Commit

---
+++ Weather StopTests/Weather_StopTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_StopTests.swift
+//  Weather StopTests
+//
+//  Created by Austin Blaser on 9/1/18.
+//  Copyright © 2018 Austin Blaser. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Stop
+
+class Weather_StopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
