b9ffd25b76a8d50344b861d503243a31ea619089
AustinDev16 <austin.blaser@gmail.com>
2018-09-01 12:42:30 -0600

    Added first properties, methods to configure view.

--- Weather Stop/View Controllers/MainWeatherViewController.swift
+++ Weather Stop/View Controllers/MainWeatherViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class MainWeatherViewController: UIViewController {
 
+    // MARK: - Properties
+    var weatherObject: WeatherObject?
+    
+    // View
+    let tempLabel = UILabel()
+    let descriptionLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +27,9 @@ class MainWeatherViewController: UIViewController {
                 print(error!.localizedDescription)
             }
         }
+        
+        configureNavigationBar()
+        configureView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,15 +37,14 @@ class MainWeatherViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Configure View
+    func configureNavigationBar() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
+        self.navigationItem.rightBarButtonItem = refresh
     }
-    */
-
+    
+    func configureView() {
+        
+    }
 }
